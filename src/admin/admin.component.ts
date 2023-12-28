4c549d5f1388e888c21bf840cd2c3c9bee00744f
Yasmine Hossam ElDin <yasmine.hossam2001@gmail.com>
2023-12-28 17:16:50 +0200

    display multiple cars in admin component

--- src/admin/admin.component.ts
+++ src/admin/admin.component.ts
@@ -6,18 +6,19 @@ import { CarService } from "../car/car.service";
 import { Car } from "../car/car.model";
 import { response } from "express";
 import { error } from "console";
+import { CarComponent } from "../car/car.component";
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NzGridModule],
+  imports: [CommonModule, RouterOutlet, NzGridModule, CarComponent],
   providers: [CarService]
 })
 export class AdminComponent {
 
-  cars: Car[];
+  carRows: Car[][] = [];
 
   constructor(private carService: CarService) {
   }
@@ -26,6 +27,16 @@ export class AdminComponent {
     this.carService.getCars().pipe().subscribe({
       next: (response: Car[]) => {
         console.log(response);
+        for (let i = 0; i < response.length; i++) {
+          const temp = [];
+          const start = i;
+          while (i < response.length && (i == start || i % 4 != 0)) {
+            temp.push(response[i]);
+            i++;
+          }
+          this.carRows.push(temp);
+          if (temp.length == 4) i--;
+        };
       },
       error: (error) => console.log(error.error)
     });
